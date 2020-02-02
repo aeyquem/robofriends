7fff6f550150200d5ca8af01d75d6283c15cfbf0
Alejandro Eyquem <aeyquem@live.cl>
2020-02-02 21:28:19 +1100

    Styles added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
+import './App.css';
 
 
 class App extends Component {
@@ -24,7 +25,7 @@ class App extends Component {
 
         return (
             <Fragment>
-                <h1 className="tc">RoboFriends</h1>
+                <h1 className="tc f2">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}></SearchBox>
                 <CardList robots={filteredRobots} />
             </Fragment>
