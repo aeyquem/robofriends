f12e7268844a163d02d2cee9217f72064c360b13
Alejandro Eyquem <aeyquem@live.cl>
2020-02-04 02:18:54 +1100

    Created structure folder and code cleanup

---
+++ src/containers/App.js
@@ -0,0 +1,53 @@
+import React, { Component, Fragment } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                return response.json();
+            }).then(users => {
+                this.setState({ robots: users });
+            });
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value });
+    }
+
+    render() {
+        const { robots, searchField } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        });
+
+        if (!robots.length) {
+            return <h1>Loading...</h1>
+        }
+        else {
+            return (
+                <Fragment>
+                    <h1 className="tc f2">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}></SearchBox>
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </Fragment>
+            );
+        }
+    }
+}
+
+export default App;
