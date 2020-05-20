70252beda7eb334a975b30624efc2b23bd379049
Alejandro Eyquem <aeyquem@live.cl>
2020-05-20 21:36:29 +1000

    Updates

---
+++ precache-manifest.94de920a22a6336f61c3b7f16463c864.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5dff65249b038ac45ca5935a6b5e219",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ac3ec4706406ec46f7c5",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8cd022f9f85bfd201a27",
+    "url": "/robofriends/static/css/main.f8f89034.chunk.css"
+  },
+  {
+    "revision": "ac3ec4706406ec46f7c5",
+    "url": "/robofriends/static/js/2.fb344145.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/robofriends/static/js/2.fb344145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd022f9f85bfd201a27",
+    "url": "/robofriends/static/js/main.3963fedd.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
